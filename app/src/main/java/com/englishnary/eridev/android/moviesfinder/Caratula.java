9d9d78b36d5f969da639ec630053f8e285b1b8a3
eriDam <erika.c.caballero@gmail.com>
2016-01-31 19:03:53 +0100

    Add Caratula java class & generate getters and setters to access it

--- app/src/main/java/com/englishnary/eridev/android/moviesfinder/Caratula.java
+++ app/src/main/java/com/englishnary/eridev/android/moviesfinder/Caratula.java
@@ -4,4 +4,22 @@ package com.englishnary.eridev.android.moviesfinder;
  * Created by eridev on 31/01/16.
  */
 public class Caratula {
+    private String imdb;
+    private String caratula;
+
+    public String getImdb() {
+        return imdb;
+    }
+
+    public void setImdb(String imdb) {
+        this.imdb = imdb;
+    }
+
+    public String getCaratula() {
+        return caratula;
+    }
+
+    public void setCaratula(String caratula) {
+        this.caratula = caratula;
+    }
 }
